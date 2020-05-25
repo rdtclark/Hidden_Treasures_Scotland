46e35383bd2e0bb4eef4a248aaf6036d4b894ffb
Robin Clark <robindtclark@gmail.com>
2020-05-25 15:32:34 +0100

    frontend file structure created

---
+++ client/src/App.vue
@@ -0,0 +1,25 @@
+<template></template>
+
+<script>
+import ApiServices from './services/apiServices'
+
+export default {
+  name: "app",
+  data() {},
+  mounted() {
+    getData();
+  },
+  methods: {
+    getData() {
+      ApiServices.getTreasures()
+    }
+  },
+  components: {
+    Map,
+    PlaceForm
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ client/src/components/PlaceImage.vue
@@ -0,0 +1,14 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    name: 'place-container'
+
+}
+</script>
+
+<style>
+
+</style>
